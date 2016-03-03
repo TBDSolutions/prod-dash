cfff4a0255f38f1e19bfc25a70c2273b0135edfe
Joshua Hagedorn <josh.hagedorn@gmail.com>
2016-03-02 23:02:46 -0500

    Add function to process all service data

---
+++ prep/function_combineServices.R
@@ -0,0 +1,17 @@
+combineServices <- function(directory) {
+  ## 'directory' is a char vector of len 1 indicating location of CSV files
+  files <- list.files(directory,full.names = TRUE) # make list of full file names
+  n <- length(files)
+  df <- data.frame() #create empty data frame
+  for (i in 1:n) {
+    # loop through files, rbinding them together
+    library(readxl)
+    x <- read_excel(files[i], na = "NULL")
+    
+    df <- rbind(df, x)
+  }
+  
+  return(df)
+}
+
+
